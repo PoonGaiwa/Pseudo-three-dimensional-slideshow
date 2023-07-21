77fbc76ef5ad61cd55fac1f0ae7e9024a5f00762
gaiwa <gaiwa@163.com>
2023-07-21 16:36:46 +0800

    slideshow

---
+++ js/common.js
@@ -0,0 +1,21 @@
+/*
+ * @Author: gaiwa gaiwa@163.com
+ * @Date: 2023-07-18 22:54:47
+ * @LastEditors: gaiwa gaiwa@163.com
+ * @LastEditTime: 2023-07-21 16:25:00
+ * @FilePath: \html\work\js\common.js
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
+function setStyle(dom,css){
+  for (var key in css){
+    dom['style'][key] = css[key];
+  }
+}
+function getStyle (obj, attr){
+	return obj.currentStyle ? obj.currentStyle[attr] : getComputedStyle(obj, false)[attr];
+}
+
+
+var a = [1,2,3,4,5];
+a.push(a.shift());
+console.log(a)
---
+++ js/pic_switching.js
@@ -0,0 +1,104 @@
+var posData = [
+  {
+    zIndex: 0,
+    top: '-400px',
+    left: '-400px',
+    width: '600px',
+    height: 'auto',
+    boxShadow: '0 0 8px #333',
+  },
+  {
+    zIndex: 1,
+    top: '-250px',
+    left: '-700px',
+    width: '800px',
+    height: 'auto',
+    boxShadow: '0 6px 8px #333',
+  },
+  {
+    zIndex: 2,
+    top: '0px',
+    left: '0px',
+    width: '1000px',
+    height: 'auto',
+    boxShadow: '0 12px 8px #333',
+  },
+  {
+    zIndex: 1,
+    top: '-250px',
+    left: '700px',
+    width: '800px',
+    height: 'auto',
+    boxShadow: '0 6px 8px #333',
+  },
+  {
+    zIndex: 0,
+    top: '-400px',
+    left: '400px',
+    width: '600px',
+    height: 'auto',
+    boxShadow: '0 0 8px #333'
+  }
+];
+var aLi = document.querySelectorAll('.pic-list li');
+var oBtn = document.querySelector('.btn-wrap');
+var vDomArr = getVirtualArr(aLi);
+var tapMap = {
+  'prev': function(){
+    vDomArr.push(vDomArr.shift());
+  },
+  'next': function(){
+    vDomArr.unshift(vDomArr.pop());
+  }
+}
+translate(aLi);
+oBtn.addEventListener('click',function(e){
+  if(e.target.tagName.toLowerCase() === 'span'){
+    var btnType = e.target.className;
+    if (tapMap[btnType] && typeof tapMap[btnType] === 'function'){
+      tapMap[btnType]();
+      translate(vDomArr);
+    }
+  }
+})
+function translate(translateTarget){
+  posData.forEach(function(item, idx, arr){
+    animate(translateTarget[idx],item);
+  });
+}
+function animate(ele,json,callback){
+  clearInterval(ele.time);
+  var toggle = false;
+  var curr, target;
+  ele.time = setInterval(function(){
+    toggle = true;
+    for (var key in json){
+      target = parseInt(json[key]);
+      curr = parseInt(getStyle(ele,key));
+      speed = (target - curr) / 50;
+      speed = speed > 0 ? Math.ceil(speed) : Math.floor(speed);
+      if (key === 'zIndex' || key === 'boxShadow'){
+        ele.style[key] = json[key];
+        continue;
+      }
+      if(curr === target) {
+        ele.style[key] = target + 'px';
+      }
+      ele.style[key] = curr + speed + 'px';
+      if (curr !== target){
+        toggle = false;
+      }
+    }
+    if (toggle) {
+      clearInterval(ele.time);
+      callback && callback();
+    }
+  });
+}
+function getVirtualArr(nodeList){
+  var newArr = [];
+  for (var i = 0, len = nodeList.length; i<len; i++){
+    newArr[i] = nodeList[i];
+  }
+  return newArr;
+}
